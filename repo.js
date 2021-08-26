766e117f332fb852726ad0c361ea309a80cbc871
bopoh13 <bopoh13@ya.ru>
2021-08-26 20:01:30 +0300

    Test 6.2

--- repo.js
+++ repo.js
@@ -1,5 +1,7 @@
 "use strict";
 
+// https://cakeinpanic.medium.com/github-actions-пишем-свой-8eb0a05c3e8a
+
 import { context, GitHub } from '@actions/github/lib/github';
 import * as core from '@actions/core'; const TOKEN = core.getInput('github-token', {required: true}); // client может использовать весть REST api от octokit https://octokit.github.io/rest.js/v18
 const client = new GitHub(TOKEN); // context содержит всю информацию 

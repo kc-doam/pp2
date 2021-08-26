69868b98c95e08b9ac74552f63fb0e83e6fbcadd
bopoh13 <bopoh13@ya.ru>
2021-08-26 20:24:43 +0300

    Test 6.6.2

--- repo.js
+++ repo.js
@@ -1,18 +1,43 @@
 "use strict";
 
-// https://cakeinpanic.medium.com/github-actions-пишем-свой-8eb0a05c3e8a
+// https://medium.com/nuances-of-programming/github-actions-начало-38e47a656ff2
 
-import { context, GitHub } from '@actions/github/lib/github';
-import * as core from '@actions/core'; const TOKEN = core.getInput('github-token', {required: true}); // client может использовать весть REST api от octokit https://octokit.github.io/rest.js/v18
-const client = new GitHub(TOKEN); // context содержит всю информацию 
-const {
-  payload: {
-    repository,
-    organization: { login: owner },
-    pull_request: pullRequest,
-  },
-} = context;const isPRMerged = await octokit.pulls.checkIfMerged({
-  owner,
-  repo: repository.name,
-  pull_number: pullRequest.number
-});
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+async function run() {
+  try {
+    const accessToken = core.getInput('access-token');
+    const message = core.getInput('message');
+
+    const payload = github.context.payload;
+    const githubClient = github.getOctokit(accessToken);
+    core.info("Request received");
+
+    if (payload.action === "opened") {
+      core.info("New Pull Request..");
+      const pullRequest = payload.pull_request;
+      const userName = pullRequest.user.login;
+      const owner = pullRequest.base.repo.owner.login;
+      const repoName = pullRequest.base.repo.name;
+      const issueNumber = pullRequest.number;
+      const comment = message.replace(/{}/g, userName);
+
+      const shouldComment = await isFirstPull(
+        githubClient, owner, repoName,
+        userName, issueNumber
+      );
+
+      // Отправляем комментарий к пул реквесту нового участника
+      if (shouldComment) {
+        core.info("Commenting");
+        githubClient.issues.createComment({ owner, repo: repoName, issue_number: issueNumber, body: comment });
+      }
+    }
+  } catch (err) {
+    core.setFailed(err.message);
+  }
+
+}
+
+run();
